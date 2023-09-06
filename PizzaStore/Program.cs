aad8a3d3de1aa2b130ce1f74c07fd9a4f6ed7e78
= <alvesil.eduardo.silva@gmail.com>
2023-09-06 00:49:38 -0300

    projeto finalizado

--- PizzaStore/Program.cs
+++ PizzaStore/Program.cs
@@ -3,8 +3,11 @@ using Microsoft.EntityFrameworkCore;
 using PizzaStore.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("Pizzas") ?? "Data Source=Pizzas.db";
 
-builder.Services.AddDbContext<PizzaDb>(options => options.UseInMemoryDatabase("items"));
+/* Banco de Dados em Memória */
+// builder.Services.AddDbContext<PizzaDb>(options => options.UseInMemoryDatabase("items"));
+builder.Services.AddSqlite<PizzaDb>(connectionString);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
