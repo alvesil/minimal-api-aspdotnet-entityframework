f9706f4d8a8a97869c5cc37baa6ff76cf7208183
= <alvesil.eduardo.silva@gmail.com>
2023-09-06 00:18:26 -0300

    Adicionando verbos http para o banco de dados em memória

--- PizzaStore/Program.cs
+++ PizzaStore/Program.cs
@@ -25,5 +25,28 @@ app.UseSwaggerUI(c =>
 
 app.MapGet("/", () => "Hello World!");
 app.MapGet("/pizzas", async (PizzaDb db) => await db.Pizzas.ToListAsync());
-
+app.MapGet("/pizza/{id}", async (PizzaDb db, int id) => await db.Pizzas.FindAsync(id));
+app.MapPost("/pizza", async (PizzaDb db, Pizza pizza) =>
+{
+    await db.Pizzas.AddAsync(pizza);
+    await db.SaveChangesAsync();
+    return Results.Created($"/pizza/{pizza.Id}", pizza);
+});
+app.MapPut("/pizza", async (PizzaDb db, Pizza updatepizza, int id) => 
+{
+    var pizza = await db.Pizzas.FindAsync(id);
+    if (pizza is null) return Results.NotFound();
+    pizza.Name = updatepizza.Name;
+    pizza.Description = updatepizza.Description;
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+app.MapDelete("/pizza", async (PizzaDb db, int id) => 
+{
+    var pizza = await db.Pizzas.FindAsync(id);
+    if (pizza is null) return Results.NotFound();
+    db.Pizzas.Remove(pizza);
+    await db.SaveChangesAsync();
+    return Results.Ok();
+});
 app.Run();
